7c983a5e62e5c8ab596cbcedf8347f57b2d95cc3
YeYuRaoXinXian <2532316972@qq.com>
2025-09-20 05:53:40 +0800

    Update startqemu.sh

--- alpine/startqemu.sh
+++ alpine/startqemu.sh
@@ -1,3 +1,6 @@
+# 获取脚本所在目录并切换到该目录
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
 qemu-system-x86_64 -machine q35 -m 8192 -smp cpus=6 -cpu qemu64 \
   -drive if=pflash,format=raw,read-only,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
   -netdev user,id=n1,hostfwd=tcp::2222-:22,net=192.168.50.0/24 -device virtio-net,netdev=n1 \
